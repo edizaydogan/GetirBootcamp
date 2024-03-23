d093d447515d6513598fd43563d3bc189ca3be0d
Ediz Aydogan <edizaydogan@Edizs-MacBook-Air.local>
2024-03-23 06:55:45 +0300

    Added hw of week1

--- myScript.swift
+++ myScript.swift
@@ -2,7 +2,7 @@
 import Foundation
 //print("Merhaba, Getir! Swift Bootcamp ")
 
-//var users = ["sinem", "rezzak", "merve", "ahmet", "ecem", "swift"]
+var users = ["sinem", "rezzak", "merve", "ahmet", "ecem", "swift"]
 
 /*if users.contains("") {
 	print("Error! There is an empty user name.")
@@ -12,8 +12,13 @@ import Foundation
 	}
 }*/
 
-print("Enter your name:")
+/*print("Enter your name:")
 var name = readLine(strippingNewline: true)
-print("Hello \(name ?? "anonymous")")
+print("Hello \(name ?? "anonymous")")*/
 
 //Ödev: users ları tüm harflerini büyütüp Z - A ya sıralı olacak şekilde yazınız...
+
+let sortedUsers = users.map { $0.uppercased() }.sorted(by: { $0 > $1 })
+
+print(sortedUsers)
+
